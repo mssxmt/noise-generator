67c69273eb2a109eb59d6201124fa525a00f11d9
mssxmt <orela.rec@gmail.com>
2024-07-15 19:07:07 +0900

    clientComponentのラッパー

---
+++ components/ClientWrapper/ClientWrapper.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+
+export const ClientWrapper: React.FC = () => {
+  // クライアントサイドの状態管理ロジック
+
+  return (
+    <>
+      {/* ここに必要なクライアントサイドのロジックや状態を持つコンポーネントを配置 */}
+    </>
+  );
+};
