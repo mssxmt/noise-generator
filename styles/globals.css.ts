cd5bbcedab5f0a24c5cb223ac31d40b52859cc1e
mssxmt <orela.rec@gmail.com>
2024-07-15 19:07:42 +0900

    グローバルcss

---
+++ styles/globals.css.ts
@@ -0,0 +1,11 @@
+import { globalStyle } from '@vanilla-extract/css';
+
+globalStyle('body', {
+  margin: 0,
+  padding: 0,
+  fontFamily: 'Arial, sans-serif',
+});
+
+globalStyle('h1, h2', {
+  color: '#333',
+});
