af8b980e8f6eaefa8d76f1e235f8db476ebe05f3
mssxmt <orela.rec@gmail.com>
2024-07-21 10:29:58 +0900

    メインコンポーネント追加

--- components/ClientWrapper/ClientWrapper.tsx
+++ components/ClientWrapper/ClientWrapper.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import React from 'react';
+import { Generator } from '../Generator/Generator';
 
 export const ClientWrapper: React.FC = () => {
   // クライアントサイドの状態管理ロジック
 
   return (
     <>
+      <Generator />
       {/* ここに必要なクライアントサイドのロジックや状態を持つコンポーネントを配置 */}
     </>
   );
