03eab6897a070c5bf3faba7b24ec528fd3b3e330
mssxmt <orela.rec@gmail.com>
2024-07-23 00:11:36 +0900

    ページ大枠調整

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,32 @@
-import { ClientWrapper } from '@/components/ClientWrapper/ClientWrapper';
+import { Generator } from '@/components/Generator/Generator';
 import { NoiseGenerator } from '@/components/NoiseGenerator/NoiseGenerator';
+import { css } from '@kuma-ui/core';
 
+const Main = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /* justify-content: center; */
+  min-height: 100vh;
+  padding: 0 5rem;
+  color: t('colors.primary.default');
+`;
+
+const Section = css`
+  width: 100%;
+  max-width: 800px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
 export default function Home() {
   return (
-    <main>
-      <h1>ノイズジェネレーター</h1>
-      <NoiseGenerator>
-        <ClientWrapper />
-      </NoiseGenerator>
+    <main className={Main}>
+      <section className={Section}>
+        <NoiseGenerator>
+          <Generator />
+        </NoiseGenerator>
+      </section>
     </main>
   );
 }
