63d7ec1737aa7462e2c1fa69feb1ae5512ef3035
mssxmt <orela.rec@gmail.com>
2024-07-23 00:24:11 +0900

    軽微な変更

--- components/ClientWrapper/ClientWrapper.tsx
+++ components/ClientWrapper/ClientWrapper.tsx
@@ -2,12 +2,14 @@
 
 import React from 'react';
 import { Generator } from '../Generator/Generator';
+import { ThemeToggle } from '../ThemeToggle';
 
 export const ClientWrapper: React.FC = () => {
   // クライアントサイドの状態管理ロジック
 
   return (
     <>
+      <ThemeToggle />
       <Generator />
       {/* ここに必要なクライアントサイドのロジックや状態を持つコンポーネントを配置 */}
     </>
