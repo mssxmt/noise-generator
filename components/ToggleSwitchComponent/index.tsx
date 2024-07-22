a26352738c732ba510323293a9ace8a177a697ce
mssxmt <orela.rec@gmail.com>
2024-07-23 00:41:18 +0900

    トグルボタンコンポーネント

---
+++ components/ToggleSwitchComponent/index.tsx
@@ -0,0 +1,118 @@
+import { css } from '@kuma-ui/core';
+import { IconSatellite, IconPlanet } from '@tabler/icons-react';
+import { ChangeEvent } from 'react';
+
+const Switch = css`
+  grid-column: 1 / 2;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 3rem;
+  justify-self: center;
+  input {
+    position: absolute;
+    opacity: 0;
+    visibility: hidden;
+  }
+  & input:checked {
+    & ~ label {
+      &::before {
+        opacity: 0.5;
+      }
+      &::after {
+        left: 57%;
+        background: var(--greyLight-1);
+      }
+    }
+  }
+`;
+
+const Input = css`
+  width: 6rem;
+  label {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 3rem;
+    box-shadow: t('colors.shadow');
+    background: rgba(255, 255, 255, 0);
+    position: relative;
+    cursor: pointer;
+    border-radius: 1.6rem;
+
+    &::after {
+      content: '';
+      position: absolute;
+      left: 0.4rem;
+      width: 2.1rem;
+      height: 2.1rem;
+      border-radius: 50%;
+      background: var(--greyDark);
+      transition: all 0.4s ease;
+    }
+    &::before {
+      content: '';
+      width: 100%;
+      height: 100%;
+      border-radius: inherit;
+      background: linear-gradient(
+        330deg,
+        var(--primary-dark) 0%,
+        var(--primary) 50%,
+        var(--primary-light) 100%
+      );
+      opacity: 0;
+      transition: all 0.4s ease;
+    }
+  }
+`;
+
+const IconWrapper = css`
+  position: absolute;
+  top: 50%;
+  left: 0.4rem;
+  transform: translateY(-50%);
+  transition: all 0.4s ease;
+  z-index: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 2.1rem;
+  height: 2.1rem;
+  border-radius: 50%;
+`;
+
+type Props = {
+  checked: boolean;
+  setChecked: (checked: boolean) => void;
+};
+export const ToggleSwitch = ({ checked, setChecked }: Props) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setChecked(e.target.checked);
+  };
+
+  return (
+    <>
+      <div className={Switch}>
+        <div className={Input}>
+          <input
+            id='switch-1'
+            type='checkbox'
+            checked={checked}
+            onChange={handleChange}
+          />
+          <label htmlFor='switch-1'>
+            <div
+              className={IconWrapper}
+              style={{
+                left: checked ? '57%' : '0.4rem',
+                color: checked ? 'var(--primary-dark)' : 'var(--greyLight-1)',
+              }}
+            >
+              {checked ? <IconPlanet size={18} /> : <IconSatellite size={18} />}
+            </div>
+          </label>
+        </div>
+      </div>
+    </>
+  );
+};
