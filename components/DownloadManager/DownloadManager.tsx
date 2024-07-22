a2f86b7b5a25ac1ef1d0c6fc0b46ead562f4b448
mssxmt <orela.rec@gmail.com>
2024-07-23 00:26:23 +0900

    スタイル追加

--- components/DownloadManager/DownloadManager.tsx
+++ components/DownloadManager/DownloadManager.tsx
@@ -9,6 +9,37 @@ type DownloadManagerProps = {
   noiseFiles: StoredNoise[];
 };
 
+const Button = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+  outline: none;
+  border: none;
+  width: fit-content;
+  height: 4rem;
+  padding: 8px 16px;
+  border-radius: 1rem;
+  box-shadow: t('colors.shadow');
+  background-color: transparent; //t('colors.grey.light1');
+
+  cursor: pointer;
+  transition: 0.3s ease;
+  text-align: center;
+
+  color: t('colors.grey.dark');
+  &:hover:not(:disabled) {
+    color: t('colors.primary.default');
+    box-shadow: t('colors.shadowHover');
+  }
+  &:active:not(:disabled) {
+    box-shadow: t('colors.innerShadow');
+  }
+  &:disabled {
+    opacity: 0.5;
+  }
+`;
+
 const DownloadManager: React.FC<DownloadManagerProps> = ({ noiseFiles }) => {
   const handleDownload = async () => {
     if (noiseFiles.length === 0) {
