f5307a9ae126e573cdbc7f92c98d65651da0958c
mssxmt <orela.rec@gmail.com>
2024-07-26 01:35:22 +0900

    metaデータ追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,45 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import localFont from 'next/font/local';
 import '../styles/globals.css';
 import { KumaRegistry } from '@kuma-ui/next-plugin/registry';
 import { ThemeProvider } from '@/providers/NextThmesProvider';
 import { Header } from '@/components/Header';
 import { ThemeToggle } from '@/components/ThemeToggle';
-const inter = Inter({ subsets: ['latin'] });
+
+const siteName = 'NOISE XENERATOR';
+const description = `no problem! Let's generate noise!`;
+const url = 'https://noise-xenerator.vercel.app/';
 
 export const metadata: Metadata = {
-  title: 'Noise Generator',
-  description: 'Generate various types of noise',
+  // 指定が必要 https://docs.netlify.com/configure-builds/environment-variables/
+  metadataBase: new URL('https://noise-xenerator.vercel.app/'),
+  description,
+  title: {
+    default: siteName,
+    /** `next-seo`の`titleTemplate`に相当する機能 */
+    template: `%s - ${siteName}`,
+  },
+  openGraph: {
+    title: siteName,
+    description,
+    url,
+    siteName,
+    locale: 'ja_JP',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteName,
+    description,
+    site: '@marip13',
+    creator: '@marpi13',
+  },
+  verification: {
+    google: 'サーチコンソール',
+  },
+  alternates: {
+    canonical: url,
+  },
 };
 const exanRegular = localFont({
   src: '../public/fonts/Exan-Regular.ttf',
