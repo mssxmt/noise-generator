bf291b1b53472b64f2227bfe43bdc9f5b5b40674
mssxmt <orela.rec@gmail.com>
2024-07-31 12:01:46 +0900

    useRef追加

--- components/Generator/Generator.tsx
+++ components/Generator/Generator.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useCallback, useEffect, useRef } from 'react';
 import {
   generateNoise,
   NoiseType,
