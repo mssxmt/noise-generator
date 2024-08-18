e908d33a79cc1746f08e6a87774aa5ceb5897068
mssxmt <orela.rec@gmail.com>
2024-08-18 14:13:30 +0900

    バージョン1.5.1

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -43,7 +43,7 @@ export const Header = ({ children }: { children?: React.ReactNode }) => {
         <IconWaveSine />
         <div className={Title}>
           <h1 className={H1}>NOISE GENERATOR</h1>
-          <p>1.5.0</p>
+          <p>1.5.1</p>
         </div>
         <IconWaveSquare />
       </div>
